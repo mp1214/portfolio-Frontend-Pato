9fa1b2dc9d919073a3a3fc1d2b4ebcc062b1cf97
mp1214 <madepato3@gmail.com>
2022-11-26 19:51:59 -0300

    se agrego guard a las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
+import { GuardianGuard } from './guard/guardian.guard';
 import { IntroComponent } from './intro/intro.component';
 
 const routes: Routes = [
 {path: 'intro', component:IntroComponent},
-{path: 'dashboard', component:DashboardComponent},
+{path: 'dashboard', component:DashboardComponent, canActivate:[GuardianGuard]},
 {path: '', redirectTo: '/intro', pathMatch:'full'},
 {path:'**', component:ErrorComponent}];
 
---
+++ src/app/guard/guardian.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenService } from '../servicios/token.service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardianGuard implements CanActivate {
+  isLogged=false;
+  constructor(private tokenService:TokenService,private router: Router) {    }
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    this.tokenService.getUserName()
+    if(this.tokenService.getToken()&& this.tokenService.getUserName()=='admin'){
+      this.isLogged=true;
+      return true;
+    }else{
+      this.router.navigate(['/intro']);
+      this.isLogged=false;
+      return false;
+      
+    }
+  }
+  
+}
